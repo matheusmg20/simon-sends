c0c9056cfc060eded420eb40f85f051116eb57a8
Matheus Mesquita Graciano <matheus.graciano@grendene.com.br>
2025-11-10 15:31:37 -0300

    adicionando subdivisão por unidades

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ CORS(app)  # Permite requisições de outros domínios (Live Server)
 @app.route("/run-montagem-f7-nd", methods=["POST"])
 def run_montagem_f7_nd():
     try:
-        script_path = os.path.join(os.getcwd(), "public", "mail", "sobral", "montagem", "fabrica-7", "montagem-f7-nd.py")
+        script_path = os.path.join(os.getcwd(), "public", "mail", "unidades", "sobral", "montagem", "fabrica-7", "montagem-f7-nd.py")
         result = subprocess.run(["python", script_path], capture_output=True, text=True)
         logs = result.stdout.splitlines()  # Captura os prints do script
         return jsonify({"status": "success", "logs": logs})
@@ -21,7 +21,7 @@ def run_montagem_f7_nd():
 @app.route("/run-montagem-f7-bman", methods=["POST"])
 def run_montagem_f7_bman():
     try:
-        script_path = os.path.join(os.getcwd(), "public", "mail", "sobral", "montagem", "fabrica-7", "montagem-f7-bman.py")
+        script_path = os.path.join(os.getcwd(), "public", "mail", "unidades", "sobral", "montagem", "fabrica-7", "montagem-f7-bman.py")
         result = subprocess.run(["python", script_path], capture_output=True, text=True)
         logs = result.stdout.splitlines()
         return jsonify({"status": "success", "logs": logs})
